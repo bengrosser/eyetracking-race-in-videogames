01f6b048c25b66b7f5b66ea7a897630b941756c4
Nicole Pontaza <Elocintrocity@users.noreply.github.com>
2017-07-27 13:48:59 -0500

    Final Updates
    Better UI/UX, "Segment Data" and "Pause" yet to be implemented, but those are just small details for later use if wanted.

---
+++ DataGrabber/DataGrabber/DataGrabber.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace DataGrabber
+{
+    partial class DataGrabberWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AnalyzeGB = new System.Windows.Forms.GroupBox();
+            this.StartOverButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.process1 = new System.Diagnostics.Process();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.InProgressGB = new System.Windows.Forms.GroupBox();
+            this.saccadeBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fixateBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimeBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DataGB = new System.Windows.Forms.GroupBox();
+            this.checkBox = new System.Windows.Forms.TextBox();
+            this.AddSepButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AnalyzeGB.SuspendLayout();
+            this.InProgressGB.SuspendLayout();
+            this.DataGB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AnalyzeGB
+            // 
+            this.AnalyzeGB.Controls.Add(this.StartOverButton);
+            this.AnalyzeGB.Controls.Add(this.StopButton);
+            this.AnalyzeGB.Controls.Add(this.PauseButton);
+            this.AnalyzeGB.Controls.Add(this.StartButton);
+            this.AnalyzeGB.Location = new System.Drawing.Point(13, 13);
+            this.AnalyzeGB.Name = "AnalyzeGB";
+            this.AnalyzeGB.Size = new System.Drawing.Size(313, 137);
+            this.AnalyzeGB.TabIndex = 0;
+            this.AnalyzeGB.TabStop = false;
+            this.AnalyzeGB.Text = "Analyze";
+            // 
+            // StartOverButton
+            // 
+            this.StartOverButton.Location = new System.Drawing.Point(189, 41);
+            this.StartOverButton.Name = "StartOverButton";
+            this.StartOverButton.Size = new System.Drawing.Size(75, 23);
+            this.StartOverButton.TabIndex = 3;
+            this.StartOverButton.Text = "Start Over";
+            this.StartOverButton.UseVisualStyleBackColor = true;
+            this.StartOverButton.Click += new System.EventHandler(this.StartOverButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(189, 83);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Finish";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Location = new System.Drawing.Point(33, 83);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(75, 23);
+            this.PauseButton.TabIndex = 1;
+            this.PauseButton.Text = "Pause";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(33, 41);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // InProgressGB
+            // 
+            this.InProgressGB.Controls.Add(this.saccadeBox);
+            this.InProgressGB.Controls.Add(this.label3);
+            this.InProgressGB.Controls.Add(this.fixateBox);
+            this.InProgressGB.Controls.Add(this.label2);
+            this.InProgressGB.Controls.Add(this.TimeBox);
+            this.InProgressGB.Controls.Add(this.label1);
+            this.InProgressGB.Location = new System.Drawing.Point(11, 156);
+            this.InProgressGB.Name = "InProgressGB";
+            this.InProgressGB.Size = new System.Drawing.Size(315, 88);
+            this.InProgressGB.TabIndex = 1;
+            this.InProgressGB.TabStop = false;
+            this.InProgressGB.Text = "In Progress";
+            // 
+            // saccadeBox
+            // 
+            this.saccadeBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.saccadeBox.Location = new System.Drawing.Point(233, 48);
+            this.saccadeBox.Name = "saccadeBox";
+            this.saccadeBox.ReadOnly = true;
+            this.saccadeBox.Size = new System.Drawing.Size(66, 20);
+            this.saccadeBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(172, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Saccades:";
+            // 
+            // fixateBox
+            // 
+            this.fixateBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.fixateBox.Location = new System.Drawing.Point(75, 48);
+            this.fixateBox.Name = "fixateBox";
+            this.fixateBox.ReadOnly = true;
+            this.fixateBox.Size = new System.Drawing.Size(66, 20);
+            this.fixateBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fixations:";
+            // 
+            // TimeBox
+            // 
+            this.TimeBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.TimeBox.Location = new System.Drawing.Point(152, 19);
+            this.TimeBox.Name = "TimeBox";
+            this.TimeBox.ReadOnly = true;
+            this.TimeBox.Size = new System.Drawing.Size(80, 20);
+            this.TimeBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time Elapsed:";
+            // 
+            // DataGB
+            // 
+            this.DataGB.Controls.Add(this.label4);
+            this.DataGB.Controls.Add(this.checkBox);
+            this.DataGB.Controls.Add(this.AddSepButton);
+            this.DataGB.Controls.Add(this.SaveButton);
+            this.DataGB.Location = new System.Drawing.Point(13, 250);
+            this.DataGB.Name = "DataGB";
+            this.DataGB.Size = new System.Drawing.Size(313, 122);
+            this.DataGB.TabIndex = 2;
+            this.DataGB.TabStop = false;
+            this.DataGB.Text = "Data";
+            // 
+            // checkBox
+            // 
+            this.checkBox.Location = new System.Drawing.Point(73, 84);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(224, 20);
+            this.checkBox.TabIndex = 2;
+            // 
+            // AddSepButton
+            // 
+            this.AddSepButton.Location = new System.Drawing.Point(33, 38);
+            this.AddSepButton.Name = "AddSepButton";
+            this.AddSepButton.Size = new System.Drawing.Size(87, 23);
+            this.AddSepButton.TabIndex = 1;
+            this.AddSepButton.Text = "Segment Data";
+            this.AddSepButton.UseVisualStyleBackColor = true;
+            this.AddSepButton.Click += new System.EventHandler(this.AddSepButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(189, 38);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(91, 23);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Validate Data";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Validation:";
+            // 
+            // DataGrabberWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 384);
+            this.Controls.Add(this.DataGB);
+            this.Controls.Add(this.InProgressGB);
+            this.Controls.Add(this.AnalyzeGB);
+            this.MaximizeBox = false;
+            this.Name = "DataGrabberWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataGrabber";
+            this.TopMost = true;
+            this.AnalyzeGB.ResumeLayout(false);
+            this.InProgressGB.ResumeLayout(false);
+            this.InProgressGB.PerformLayout();
+            this.DataGB.ResumeLayout(false);
+            this.DataGB.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox AnalyzeGB;
+        private System.Windows.Forms.Button StartOverButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.GroupBox DataGB;
+        private System.Windows.Forms.Button AddSepButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.GroupBox InProgressGB;
+        private System.Windows.Forms.TextBox TimeBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox fixateBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox checkBox;
+        private System.Windows.Forms.TextBox saccadeBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DataGrabber/DataGrabber/DataGrabber.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using Tobii.Interaction;
+
+namespace DataGrabber
+{
+    public partial class DataGrabberWindow : Form, IDisposable
+    {
+#pragma warning disable 0169
+
+        private int min = 0;
+        private int second =-1;
+        private int fixcount = 0;
+        private int saccount = 0;
+        //private bool segmented = false;
+        private Host hostFIX;
+        private Host hostSAC;
+        private FixationDataStream fixationDataStream;
+        private GazePointDataStream gazePointDataStream;
+        //System.IO.StreamWriter file;
+        FileStream fsFIX;
+        FileStream fsSAC;
+        StreamWriter swFIX;
+        StreamWriter swSAC;
+
+        public DataGrabberWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            StartOverButton_Click(sender, e);
+            fsFIX = new FileStream(@"C: \Users\CTS\Desktop\testFIX.txt", FileMode.Create, FileAccess.Write);
+            fsSAC = new FileStream(@"C: \Users\CTS\Desktop\testSAC.txt", FileMode.Create, FileAccess.Write);
+            swFIX = new StreamWriter(fsFIX);
+            swSAC = new StreamWriter(fsSAC);
+            hostFIX = new Host();
+            hostSAC = new Host();
+
+            fixationDataStream = hostFIX.Streams.CreateFixationDataStream(Tobii.Interaction.Framework.FixationDataMode.Slow);
+            gazePointDataStream = hostSAC.Streams.CreateGazePointDataStream();
+
+            fixationDataStream.Data((x, y, timestamp) => addToTXTFIX(x, y, timestamp));
+            gazePointDataStream.GazePoint((x, y, ts) => addToTXTSAC(x, y, ts));
+        }
+
+        public void addToTXTFIX(double x, double y, double t)
+        {
+            second = (int)(t / 1000) % 60;
+            fixcount++;
+            MethodInvoker miFIX = delegate
+                {
+                    fixateBox.Text = fixcount + " pts";
+                    try { swFIX.WriteLine(t + "," + x + "," + y); }
+                    catch(Exception)
+                    {
+                        checkBox.Text = "Error FIX. 1 line went missing";
+                    }
+                    
+                };
+            if (InvokeRequired)
+                this.Invoke(miFIX);
+        }
+
+        public void addToTXTSAC(double x, double y, double t)
+        {
+            if (second == 59)
+                Interlocked.Increment(ref min);
+            second = (int)(t / 1000) % 60;
+            saccount++;
+            MethodInvoker miSAC = delegate
+            {
+                if(min == 0)
+                    TimeBox.Text = min + "m " + second + "sec";
+                else
+                {
+                    TimeBox.Text = (min/89) + "m " + second + "sec";
+                }
+
+                saccadeBox.Text = saccount + "pts";
+                try { swSAC.WriteLine(t + "," + x + "," + y); }
+                catch(Exception)
+                {
+                    checkBox.Text = "Error SAC. 1 line went missing.";
+                }
+            };
+            if (InvokeRequired)
+                this.Invoke(miSAC);
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            hostFIX.DisableConnection();
+            hostSAC.DisableConnection();
+
+            swFIX.Close();
+            fsFIX.Close();
+
+            swSAC.Close();
+            fsSAC.Close();
+
+            DialogResult d = MessageBox.Show("Data is in Desktop", "Done", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int lineCountFIX = File.ReadLines(@"C: \Users\CTS\Desktop\testFIX.txt").Count();
+                int lineCountSAC = File.ReadLines(@"C: \Users\CTS\Desktop\testSAC.txt").Count();
+                checkBox.Text = "Lines in FIX: " + lineCountFIX + "\tLines in SAC: "+lineCountSAC;
+            }
+            catch
+            {
+                DialogResult d = MessageBox.Show("Click 'Finish' before you save! Your data retrieval is still running.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StartOverButton_Click(object sender, EventArgs e)
+        {
+            fixcount = 0;
+            saccount = 0;
+            min = 0;
+            checkBox.Text = "";
+            TimeBox.Text = "";
+            fixateBox.Text = "";
+            saccadeBox.Text = "";
+            try
+            {
+                File.WriteAllText(@"C: \Users\CTS\Desktop\testFIX.txt", String.Empty);
+                File.WriteAllText(@"C: \Users\CTS\Desktop\testSAC.txt", String.Empty);
+            }
+            catch { }
+            
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            DialogResult d = MessageBox.Show("Haven't fully implemented this yet.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void AddSepButton_Click(object sender, EventArgs e)
+        {
+            DialogResult d = MessageBox.Show("Haven't fully implemented this yet.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //swFIX.WriteLine("===================");
+            //swSAC.WriteLine("===================");
+            //segmented = true;
+        }
+    }
+}
